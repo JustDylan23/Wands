f747e1f984d1f7efda0baef3c3b348b43c2caf15
JustDylan23 <dylanvdhout@gmail.com>
2026-01-07 23:02:24 +0100

    feat(Command): Remove unused utility functions

--- src/main/java/me/dylan/wands/command/CommandUtils.java
+++ src/main/java/me/dylan/wands/command/CommandUtils.java
@@ -19,15 +19,6 @@ public final class CommandUtils {
     private CommandUtils() {
     }
 
-    public static boolean isPlayerOrNotify(CommandSender sender) {
-        if (sender instanceof Player) {
-            return true;
-        } else {
-            sender.sendMessage(WandsPlugin.PREFIX + "§cOnly players can perform this command!");
-            return false;
-        }
-    }
-
     public static boolean isWandOrNotify(CommandSender sender, ItemStack itemStack) {
         if (ItemTag.IS_WAND.isTagged(itemStack)) {
             return true;
@@ -37,24 +28,6 @@ public final class CommandUtils {
         }
     }
 
-    public static boolean checkPermOrNotify(@NotNull CommandSender commandSender, String permission) {
-        if (commandSender.hasPermission(permission)) {
-            return true;
-        } else {
-            commandSender.sendMessage("§cYou require §r" + permission);
-            return false;
-        }
-    }
-
-    @Contract("_, null, _ -> false")
-    public static boolean isSpellOrNotify(@NotNull CommandSender commandSender, @Nullable SpellType spellType, String argument) {
-        if (spellType == null) {
-            commandSender.sendMessage(WandsPlugin.PREFIX + "§7§l" + argument + " §ris not a spell!");
-            return false;
-        }
-        return true;
-    }
-
     public static boolean isInRangeOrNotify(@NotNull CommandSender commandSender, int in) {
         if (in >= 0 && in <= 99) {
             return true;
@@ -62,13 +35,5 @@ public final class CommandUtils {
             commandSender.sendMessage(in + " must be a number between " + 0 + " and " + 99);
             return false;
         }
-    }
-
-    public static List<String> validCompletions(@NotNull String toComplete, String... values) {
-        String toCompleteLowered = toComplete.toLowerCase();
-        return Arrays.stream(values)
-                .map(String::toLowerCase)
-                .filter(s -> s.contains(toCompleteLowered))
-                .collect(Collectors.toList());
     }
 }
